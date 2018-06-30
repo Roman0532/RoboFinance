9972f1f72d7b12e3417a2a841fc9c5546fce3062
Роман Максимов <roman62477@gmail.com>
2018-06-30 16:53:28 +0700

    creating imageService creating to work with images

---
+++ app/Services/ImageService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\Services;
+
+class ImageService
+{
+    public function uploadImageOnServer($file)
+    {
+        $format = pathinfo($file, PATHINFO_EXTENSION);
+        $success = false;
+
+        foreach (["jpg", "jpeg", "png", "svg"] as $value) {
+            if (strtolower($format) == $value) {
+                $success = true;
+            }
+        }
+
+        if (!$success) {
+            return false;
+        }
+
+        $filename = md5($file . time()) . '.' . $format;
+        $uploadDir = 'public/storage/';
+        $uploadFile = $uploadDir . $filename;
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)) {
+            return $filename;
+        }
+        return false;
+    }
+
+    public function deleteImageFromServer($file)
+    {
+        unlink('public/storage/' . $file);
+    }
+}
